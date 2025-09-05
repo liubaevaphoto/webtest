e8ea8688a54afb1c014e053cb5549bf71b8fbc86
liubaevaphoto <rolandkromka476@gmail.com>
2025-09-05 11:46:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Select hero section
+const hero = document.querySelector(".hero");
+
+// Listen to scroll
+window.addEventListener("scroll", () => {
+  let offset = window.scrollY;
+  // Move background slower than scroll for parallax
+  hero.style.backgroundPositionY = offset * 0.5 + "px";
+});
 
